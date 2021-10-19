a3f9e0a8acce950fa0beb8ba1bc4e9f04a4fcfb0
Romina <romina.dilanzo@gire.com>
2021-10-19 00:04:00 -0300

    romina

---
+++ src/main/java/lippia/web/steps/EjemploPracticaSteps.java
@@ -0,0 +1,25 @@
+package lippia.web.steps;
+
+import com.crowdar.core.PageSteps;
+import io.cucumber.java.en.*;
+import lippia.web.services.EjemploPracticaService;
+
+public class EjemploPracticaSteps extends PageSteps {
+
+    @Given("el usuario navega a la web de automation practice")
+    public void elUsuarioNavegaALaWebDeAutomationPractice() {
+        EjemploPracticaService.navegarWeb();
+
+    }
+
+    @When("^el usuario pulsa el boton \"(.*)\"$")
+    public void elUsuarioPulsaElBoton(String boton) {
+        EjemploPracticaService.clickButton();
+
+    }
+
+    @Then("el usuario verifica que se enseña la pagina de Sign in")
+    public void elUsuarioVerificaQueSeEnseñaLaPaginaDeSignIn() {
+        EjemploPracticaService.verificarSignInPage();
+    }
+}
---
+++ src/test/resources/web/features/EjemploPractica.feature
@@ -0,0 +1,7 @@
+Feature: como usuario, quiero navegar al Sign in de la web de automation practice
+
+  @Smoke @romi
+  Scenario: El usuario navega a "Sign in"
+    Given el usuario navega a la web de automation practice
+    When el usuario pulsa el boton "Sign in"
+    Then el usuario verifica que se enseña la pagina de Sign in
