558d8b160c05d0d4d04aa704c575f01ef408237a
Romina <romina.dilanzo@gire.com>
2021-10-19 19:04:00 -0300

    romina

--- src/main/java/lippia/web/services/EjemploPracticaService.java
+++ src/main/java/lippia/web/services/EjemploPracticaService.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 
 import static com.crowdar.core.actions.WebActionManager.navigateTo;
 
+
 public class EjemploPracticaService extends ActionManager {
 
     public static void navegarWeb() {
@@ -21,6 +22,40 @@ public class EjemploPracticaService extends ActionManager {
         waitVisibility(EjemploPracticaConstants.INPUT_ADDRESS_XPATH);
         Assert.assertTrue(isVisible(EjemploPracticaConstants.INPUT_ADDRESS_XPATH));
         waitVisibility(EjemploPracticaConstants.H1_TITULO_XPATH);
-        Assert.assertEquals(getText(EjemploPracticaConstants.H1_TITULO_XPATH),"AUTHENTICATION","No se encuentra el titulo");
+        Assert.assertEquals(getText(EjemploPracticaConstants.H1_TITULO_XPATH), "AUTHENTICATION", "No se encuentra el titulo");
+    }
+
+    public static void verificarLoginOk(String texto) {
+        waitVisibility(EjemploPracticaConstants.H2_TITULO_XPATH);
+        Assert.assertEquals(getText(EjemploPracticaConstants.H2_TITULO_XPATH), "MY ACCOUNT", "No se encuentra el titulo");
+    }
+
+    public static void ingresarUsuario(String usuario) {
+        waitVisibility(EjemploPracticaConstants.INPUT_ADDRESS_XPATH);
+        ingresarUsuario(EjemploPracticaConstants.INPUT_ADDRESS_XPATH);
+    }
+
+    public static void ingresarPassword(String password) {
+        waitVisibility(EjemploPracticaConstants.INPUT_PASSWORD_XPATH);
+        ingresarUsuario(EjemploPracticaConstants.INPUT_PASSWORD_XPATH);
+    }
+
+    public static void clickButtonSignIn() {
+        click(EjemploPracticaConstants.BUTTON_SIGNIN_LOGIN_XPATH);
+
+    }
+
+    public static void visualizarBucador() {
+        waitVisibility(EjemploPracticaConstants.BUSCADOR_XPATH);
+
+    }
+
+    public static void realizarBusqueda(String dato) {
+        realizarBusqueda(EjemploPracticaConstants.INPUT_PASSWORD_XPATH);
+
+    }
+    public static void resultadoBusqueda(){
+        waitVisibility(EjemploPracticaConstants.RESULTADO_BUSQUEDA_XPATH);
+        Assert.assertEquals(getText(EjemploPracticaConstants.RESULTADO_BUSQUEDA_XPATH), "7 results have been found.", "No se encuentran resultados");
     }
 }
--- src/test/resources/web/features/EjemploPractica.feature
+++ src/test/resources/web/features/EjemploPractica.feature
@@ -1,7 +1,39 @@
 Feature: como usuario, quiero navegar al Sign in de la web de automation practice
 
-  @Smoke @romi
+  @Smoke
   Scenario: El usuario navega a "Sign in"
     Given el usuario navega a la web de automation practice
     When el usuario pulsa el boton "Sign in"
     Then el usuario verifica que se enseña la pagina de Sign in
+
+  @Smoke
+  Scenario Outline: El usuario navega a "Sign in"
+    Given el usuario navega a la web de automation practice
+    When el usuario pulsa el boton "Sign in"
+    And el usuario ingresa el usuario <usuario> y la Password <password>
+    Then el usuario se encuentra logueado <texto>
+
+    Examples:
+      | usuario             | password  | texto      |
+      | dilanzita@gmail.com | Gire2021* | My account |
+
+  @Smoke
+  Scenario Outline: El usuario navega a "Sign in"
+    Given el usuario navega a la web de automation practice
+    When el usuario ingresa un dato en el buscador <busqueda>
+    Then el usuario ve en pantalla los resultados de la busqueda
+
+    Examples:
+      | busqueda |
+      | dress    |
+
+  @Smoke
+  Scenario Outline: El usuario navega a "Sign in"
+    Given el usuario navega a la web de automation practice
+    When el usuario ingresa un dato en el buscador <busqueda>
+    And el usuario va a ordenar por precio mas bajo
+    Then el usuario ve en pantalla correctamente ordenado
+
+    Examples:
+      | busqueda |
+      | dress    |
